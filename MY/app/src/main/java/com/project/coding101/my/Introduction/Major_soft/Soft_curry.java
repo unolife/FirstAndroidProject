40e82b724c38e6ae02bd0571df6a032e7ffe64b9
Minji Kim <wisdomin121@naver.com>
2019-02-12 21:15:42 +0900

    finished introducing of Application Software major

---
+++ MY/app/src/main/java/com/project/coding101/my/Introduction/Major_soft/Soft_curry.java
@@ -0,0 +1,16 @@
+package com.project.coding101.my.Introduction.Major_soft;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.coding101.my.R;
+
+public class Soft_curry extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft_curry);
+    }
+}

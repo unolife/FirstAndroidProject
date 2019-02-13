85ace25d70ce93e31e90207b37dd457351f439f2
Minji Kim <wisdomin121@naver.com>
2019-02-14 00:21:58 +0900

    declared activity in manifest

--- MY/app/src/main/java/com/project/coding101/my/Introduction/Major_data/Data_Tech.java
+++ MY/app/src/main/java/com/project/coding101/my/Introduction/Major_data/Data_Tech.java
@@ -2,14 +2,20 @@ package com.project.coding101.my.Introduction.Major_data;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.project.coding101.my.R;
 
-public class Data_Tech extends AppCompatActivity {
+public class Data_Tech extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data__tech);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }

394da0c6ecf8c6008235084c0fc064443cbec107
Sunny Ahn <ahn9930@naver.com>
2019-02-18 02:37:21 +0900

    add database but not finished

--- MY/app/src/main/java/com/project/coding101/my/studentBoard/WriteActivity.java
+++ MY/app/src/main/java/com/project/coding101/my/studentBoard/WriteActivity.java
@@ -3,74 +3,144 @@ package com.project.coding101.my.studentBoard;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.project.coding101.my.R;
+import com.project.coding101.my.User;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
-public class WriteActivity extends AppCompatActivity {
+public class WriteActivity extends BaseActivity {
 
-    private EditText input_title;
-    private EditText input_content;
-    private String title;
-    private String content;
+    private static final String TAG = "NewPostActivity";
+    private static final String REQUIRED = "Required";
+
+    // [START declare_database_ref]
+    private DatabaseReference mDatabase;
+    // [END declare_database_ref]
+
+    private EditText mTitleField;
+    private EditText mBodyField;
+    private FloatingActionButton mSubmitButton;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
 
-        input_title = findViewById(R.id.title);
-        input_content = findViewById(R.id.content);
+        // [START initialize_database_ref]
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        // [END initialize_database_ref]
 
+        mTitleField = findViewById(R.id.title);
+        mBodyField = findViewById(R.id.content);
+        mSubmitButton = findViewById(R.id.add_btn);
 
-        input_title.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                title = s.toString();
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-        input_content.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                content = s.toString();
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-        Button soft_btn = (Button) findViewById(R.id.add_btn);
-        soft_btn.setOnClickListener(new View.OnClickListener() {
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Intent intent = new Intent(getApplicationContext() , writeActivity.class);
-                Intent intent = new Intent(v.getContext(), MainBoard.class);
-                startActivity(intent);
+                submitPost();
             }
         });
     }
+
+    private void submitPost() {
+        final String title = mTitleField.getText().toString();
+        final String body = mBodyField.getText().toString();
+
+        // Title is required
+        if (TextUtils.isEmpty(title)) {
+            mTitleField.setError(REQUIRED);
+            return;
+        }
+
+        // Body is required
+        if (TextUtils.isEmpty(body)) {
+            mBodyField.setError(REQUIRED);
+            return;
+        }
+
+        // Disable button so there are no multi-posts
+        setEditingEnabled(false);
+        Toast.makeText(this, "Posting...", Toast.LENGTH_SHORT).show();
+
+        // [START single_value_read]
+        final String userId = getUid();
+        mDatabase.child("users").child(userId).addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Get user value
+                        User user = dataSnapshot.getValue(User.class);
+
+                        // [START_EXCLUDE]
+                        if (user == null) {
+                            // User is null, error out
+                            Log.e(TAG, "User " + userId + " is unexpectedly null");
+                            Toast.makeText(WriteActivity.this,
+                                    "Error: could not fetch user.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Write new post
+                            writeNewPost(userId, user.nickname, title, body);
+                        }
+
+                        // Finish this Activity, back to the stream
+                        setEditingEnabled(true);
+                        finish();
+                        // [END_EXCLUDE]
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w(TAG, "getUser:onCancelled", databaseError.toException());
+                        // [START_EXCLUDE]
+                        setEditingEnabled(true);
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END single_value_read]
+    }
+
+    private void setEditingEnabled(boolean enabled) {
+        mTitleField.setEnabled(enabled);
+        mBodyField.setEnabled(enabled);
+        if (enabled) {
+            mSubmitButton.show();
+        } else {
+            mSubmitButton.hide();
+        }
+    }
+
+    // [START write_fan_out]
+    private void writeNewPost(String uid, String author, String title, String body) {
+        // Create new post at /user-posts/$userid/$postid and at
+        // /posts/$postid simultaneously
+        String key = mDatabase.child("posts").push().getKey();
+        Post post = new Post(uid, author, title, body);
+        Map<String, Object> postValues = post.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/posts/" + key, postValues);
+        childUpdates.put("/user-posts/" + uid + "/" + key, postValues);
+
+        mDatabase.updateChildren(childUpdates);
+    }
+    // [END write_fan_out]
 }

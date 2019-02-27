055319b1670a7f8e63e4f8fb154bff998442533e
sbs <unolife92@gmail.com>
2019-02-28 00:00:33 +0900

    find email, password java file

---
+++ MY/app/src/main/java/com/project/coding101/my/mypage/FindPassword.java
@@ -0,0 +1,17 @@
+package com.project.coding101.my.mypage;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.project.coding101.my.R;
+
+public class FindPassword extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.find_password, container, false);
+    }
+}

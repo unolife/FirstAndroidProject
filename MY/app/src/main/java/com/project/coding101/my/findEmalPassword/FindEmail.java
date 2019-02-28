982211ff721d78b56d45ed4b4ed9543f7daf88bc
sbs <unolife92@gmail.com>
2019-02-28 11:29:51 +0900

    modify find email

--- MY/app/src/main/java/com/project/coding101/my/findEmalPassword/FindEmail.java
+++ MY/app/src/main/java/com/project/coding101/my/findEmalPassword/FindEmail.java
@@ -1,17 +1,55 @@
 package com.project.coding101.my.findEmalPassword;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.project.coding101.my.R;
+import com.project.coding101.my.User;
 
 public class FindEmail extends Fragment {
 
+    private DatabaseReference mDatabase;
+    private DatabaseReference userDB;
+
+    EditText name, collagenum, tel;
+    Button btn_find;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.find_email, container, false);
+
+        View view1 = inflater.inflate(R.layout.find_email, container, false);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        userDB = mDatabase.child("users");
+
+        name = view1.findViewById(R.id.EditText_name);
+        collagenum = view1.findViewById(R.id.EditText_collagenum);
+        tel = view1.findViewById(R.id.EditText_tel);
+        btn_find = view1.findViewById(R.id.Button_find);
+
+        userDB.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot uniqueUserSnapshot : dataSnapshot.getChildren()){
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        return view1;
     }
 }

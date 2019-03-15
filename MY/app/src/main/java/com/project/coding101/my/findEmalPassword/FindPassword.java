a2da34bdbac6988df1a75039c51d00bf3516e43d
sbs <unolife92@gmail.com>
2019-03-15 14:51:15 +0900

    send_reset_email

--- MY/app/src/main/java/com/project/coding101/my/findEmalPassword/FindPassword.java
+++ MY/app/src/main/java/com/project/coding101/my/findEmalPassword/FindPassword.java
@@ -1,17 +1,48 @@
 package com.project.coding101.my.findEmalPassword;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.project.coding101.my.R;
 
 public class FindPassword extends Fragment {
 
+    String email;
+    Button btn_send;
+    private static final String TAG = "MainActivity";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.find_password, container, false);
+
+        View view1 = inflater.inflate(R.layout.find_password, container, false);
+
+        email = view1.findViewById(R.id.EditText_Email).toString();
+        btn_send = view1.findViewById(R.id.Button_sendEmail);
+
+        return view1;
     }
+
+    public void sendPasswordReset() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Log.d(TAG, "Email sent.");
+                        }
+                    }
+                });
+    }
+
 }
